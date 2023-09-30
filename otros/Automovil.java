ee3e05e54186bffd96c498a45e05bdc201259c97
Dante <dante.uchofen@usil.pe>
2023-09-29 21:33:27 -0500

    lab: Fundamentos de POO (UML e implementación) - Semana 07

---
+++ otros/Automovil.java
@@ -0,0 +1,23 @@
+package otros;
+
+public class Automovil {
+		private String placa;
+		private String marca= "Toyota";
+		private String modelo;
+		private String color;
+		private String tipo;
+	
+		
+		public Automovil() {
+		 super();
+		}
+
+		public String getMarca() {
+			return marca;
+		}
+
+		public String getModelo() {
+			return modelo;
+		}
+		
+}
---
+++ otros/Main.java
@@ -0,0 +1,12 @@
+package otros;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	   Automovil automovil= new Automovil();
+		
+		System.out.println(automovil.getMarca());
+	}
+
+}
